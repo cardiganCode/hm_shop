008739f5c8aa039371812c2433c903020ce1d873
cardiganCode <sunzhaoye1015@gmail.com>
2026-01-10 14:55:16 +0800

    添加主页登录页

---
+++ lib/routes/index.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:hm_shop/pages/Login/index.dart';
+import 'package:hm_shop/pages/Main/index.dart';
+
+Widget getRootWidget() {
+  return MaterialApp(initialRoute: "/", routes: getRootRoutes());
+}
+
+//返回该app的路由配置
+Map<String, Widget Function(BuildContext)> getRootRoutes() {
+  return {
+    "/": (context) => MainPage(), // 主页
+    "/login": (context) => LoginPage(), // 登录页
+  };
+}
